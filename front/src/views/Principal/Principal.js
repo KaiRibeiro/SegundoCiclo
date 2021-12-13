b07d88c3f3059dbedb6ea8ea85305e41a93a2f93
Kaique Ribeiro de Campos <kaique@forlogic.org>
2021-12-13 15:29:06 -0300

    auth

--- front/src/views/Principal/Principal.js
+++ front/src/views/Principal/Principal.js
@@ -1,12 +1,26 @@
+import "./Principal.css";
 import { Link } from "react-router-dom";
+import bigLogo from "../../public/images/bigLogo.png";
 
 function Principal() {
-    return (
+  return (
+    <main>
+      <div className="conteudo">
         <div>
-            <Link to='/login'>Login</Link>
-            <Link to='/register'>Registrar</Link>
+          <img src={bigLogo} alt="Logo" />
+          <h2>Ferramenta de acompanhamento corporal</h2>
         </div>
-    )
+        <div className="btns">
+          <button type="button" className="btn btn-primary btn-lg ">
+            <Link className="link" to="/login">Login</Link>
+          </button>
+          <button type="button" className="btn btn-lg btn-outline-primary">
+            <Link className="link" to="/register">Registrar</Link>
+          </button>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default Principal;
--- front/src/views/Register/Register.js
+++ front/src/views/Register/Register.js
@@ -1,6 +1,7 @@
 import axios from "axios";
-import { createBrowserHistory } from 'history';
+import { createBrowserHistory } from "history";
 import { useContext, useState } from "react";
+import { Link } from "react-router-dom";
 import UserContext from "../../contexts/UserContext";
 import "./Register.css";
 
@@ -19,37 +20,60 @@ function Register() {
       password,
     };
 
-    axios.post(`${process.env.REACT_APP_BACK_URL}register`, data, {withCredentials:true}).then((response) => {
-      userContext.setId(response.data.id);
-      userContext.setUsername(response.data.username);
-      if(response.data.id) {
-        history.push("/");
-      }
-    });
+    axios
+      .post(`${process.env.REACT_APP_BACK_URL}register`, data, {
+        withCredentials: true,
+      })
+      .then((response) => {
+        userContext.setId(response.data.id);
+        userContext.setUsername(response.data.username);
+        if (response.data.id) {
+          history.push("/");
+        }
+      });
   }
 
   return (
-    <div className="form">
-      <form onSubmit={(e) => registerUser(e)}>
-        <div>
-          <input
-            type="text"
-            placeholder="Usuário"
-            onChange={(e) => setUser(e.target.value)}
-          />
-        </div>
-        <div>
-          <input
-            type="password"
-            placeholder="Senha"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div className="btn">
-          <button type="submit">Registrar</button>
-        </div>
-      </form>
-    </div>
+    <main>
+      <div className="form">
+        <form onSubmit={(e) => registerUser(e)}>
+          <div className="inputGroup">
+            <label htmlFor="password" className="form-label">
+              Usuário
+            </label>
+            <input
+              id="user"
+              type="text"
+              placeholder="Usuário"
+              className="form-control"
+              onChange={(e) => setUser(e.target.value)}
+            />
+          </div>
+          <div className="inputGroup">
+            <label htmlFor="password" className="form-label">
+              Senha
+            </label>
+            <input
+              id="password"
+              type="password"
+              placeholder="Senha"
+              className="form-control"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="btn">
+            <button type="button" className="btn btn-lg btn-primary">
+              Registrar
+            </button>
+            <button type="button" className="btn btn-lg btn-primary">
+              <Link className="link" to="/">
+                Voltar
+              </Link>
+            </button>
+          </div>
+        </form>
+      </div>
+    </main>
   );
 }
 
